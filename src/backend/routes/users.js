612d4605517d9bde3fa5263bcd72bd492a13deeb
shawn edgell <shawn_edgell@hotmail.com>
2023-11-16 14:31:40 -0500

    backend setup p2

--- src/backend/routes/users.js
+++ src/backend/routes/users.js
@@ -2,22 +2,70 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
-
-// Load User model
 const User = require('../models/User');
 
-// @route   POST /api/users/register
-// @desc    Register a new user
-// @access  Public
+// Register User
 router.post('/register', (req, res) => {
-  // Registration logic
+  const { fullName, email, password, password2 } = req.body;
+  let errors = [];
+
+  // Check required fields
+  if (!fullName || !email || !password || !password2) {
+    errors.push({ msg: 'Please fill in all fields' });
+  }
+
+  // Check passwords match
+  if (password !== password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+
+  // Check pass length
+  if (password.length < 6) {
+    errors.push({ msg: 'Password should be at least 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.status(400).json({ errors, fullName, email, password, password2 });
+  } else {
+    // Validation passed
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        // User exists
+        errors.push({ msg: 'Email is already registered' });
+        res.status(400).json({ errors, fullName, email, password, password2 });
+      } else {
+        const newUser = new User({
+          fullName,
+          email,
+          password
+        });
+
+        // Hash Password
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            // Set password to hashed
+            newUser.password = hash;
+            // Save user
+            newUser.save()
+              .then(user => {
+                res.status(201).json({ message: 'You are now registered and can log in' });
+              })
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  }
 });
 
-// @route   POST /api/users/login
-// @desc    Authenticate & login
-// @access  Public
+// Login Handle
 router.post('/login', (req, res, next) => {
-  // Login logic
+  passport.authenticate('local', {
+    successRedirect: '/', // Redirect path after successful login
+    failureRedirect: '/users/login', // Redirect path if login fails
+    failureFlash: true
+  })(req, res, next);
 });
 
 module.exports = router;
