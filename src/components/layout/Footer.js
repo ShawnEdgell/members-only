13813a2634a8bc69467b108ee769dbeb0b5c38f4
shawn edgell <shawn_edgell@hotmail.com>
2023-11-17 17:56:53 -0500

    styling update

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,11 +1,10 @@
+// components/layout/Footer.js
 import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-700 text-white p-4">
-            <div className="max-w-7xl mx-auto text-center">
-                <p>© 2023 My Clubhouse</p>
-            </div>
+        <footer className="bg-gray-700 text-white p-4 text-center">
+            <p>© 2023 My Clubhouse</p>
         </footer>
     );
 };
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,6 +1,5 @@
-// Layout.js
+// components/layout/Layout.js
 import React, { useState } from 'react';
-import axios from 'axios'; // Make sure axios is imported
 import Header from './Header';
 import Footer from './Footer';
 import Modal from './Modal';
@@ -14,32 +13,34 @@ const Layout = ({ children }) => {
 
     const handleSuccessfulSignup = () => {
         setShowSignUpModal(false);
-        setShowLoginForm(true);
+        setShowLoginForm(true); // Open the login modal after successful signup
     };
 
     const handleSuccessfulLogin = () => {
         setShowLoginForm(false);
-        setIsLoggedIn(true);
+        setIsLoggedIn(true); // User is now logged in
     };
 
     const handleLogout = async () => {
         try {
             await axios.post('/api/users/logout');
-            setIsLoggedIn(false);
+            setIsLoggedIn(false); // User is now logged out
         } catch (error) {
             console.error('Logout failed:', error);
         }
     };
 
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
             <Header 
                 onOpenSignup={() => setShowSignUpModal(true)} 
                 onOpenLogin={() => setShowLoginForm(true)}
                 isLoggedIn={isLoggedIn}
                 onLogout={handleLogout}
             />
-            <main className="flex-grow">{children}</main>
+            <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-12">
+                {children}
+            </main>
             <Footer />
 
             <Modal isOpen={showSignUpModal} onClose={() => setShowSignUpModal(false)}>
@@ -48,7 +49,7 @@ const Layout = ({ children }) => {
             <Modal isOpen={showLoginForm} onClose={() => setShowLoginForm(false)}>
                 <LoginForm onSuccessfulLogin={handleSuccessfulLogin} />
             </Modal>
-        </>
+        </div>
     );
 };
 
